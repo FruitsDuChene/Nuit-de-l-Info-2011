1f0db73fc91a2f82e69129efd5ae2c9239c11f8c
Alexis Daboville <alexis.daboville@gmail.com>
2011-12-01 16:20:17 +0000

    ajout des handlers de base

--- gae2/main.py
+++ gae2/main.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python2
 #-*- coding: utf-8 -*-
 
+import os
+
+import jinja2
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
 class IndexHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('oh no')
+        self.response.out.write(
+                jinja_environment.get_template('tpl/index.html').render())
+
+class TrainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(
+                jinja_environment.get_template('tpl/train.html').render())
+
+    def post(self):
+        # data
+        self.redirect('/thanks')
+
+class ThanksHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(
+                jinja_environment.get_template('tpl/thanks.html').render())
 
 app = webapp.WSGIApplication([
                                 ('/', IndexHandler),
+                                ('/train', TrainHandler),
+                                ('/thanks', ThanksHandler),
                              ], debug=True)
 
 if __name__ == '__main__':
